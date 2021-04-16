39ae903db34bb52e6ea1357e0db9c3b3f07f3c04
jaehe <jaehe0413@naver.com>
2021-04-16 15:54:49 +0900

    Update App.js
    Router 추가

--- reactapp/src/views/App.js
+++ reactapp/src/views/App.js
@@ -1,8 +1,19 @@
+import Header from './Header'
+import Home from './Home'
+import Editor from './Editor/Editor'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 function App ()
 {
   return (
     <>
+      <Router>
+        <Header />
+        <Route exact path="/" component={Home} />
+        <Route path="/sentence" />
+        <Route path="/template" />
+        <Route Editor="/editor" component={Editor} />
+      </Router>
     </>
   );
 }
