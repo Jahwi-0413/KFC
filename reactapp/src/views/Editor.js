efb0f84fd2b85cab702f796e50a21ebcf6ebcdeb
jaehe <jaehe0413@naver.com>
2021-04-16 16:38:16 +0900

    Update Editor.js
    화면은 대강 완성

---
+++ reactapp/src/views/Editor.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import styled from 'styled-components'
+import icon from '../resources/file-upload-icon.svg'
+
+const Editor = () =>
+{
+    return (
+        <Container>
+            <Text>
+                나만의 손글씨로<br />
+            편리하게 편지를 작성해보세요<br /><br />
+            밑의 버튼을 눌러 폰트 파일을 등록하고<br />
+            편집기를 사용해 보세요<br />
+            </Text>
+            <StyledDiv>
+                <StyledInput type="file" />
+                <UploadIcon src={icon} />
+                <Text2>파일 선택</Text2>
+            </StyledDiv>
+        </Container>
+    )
+}
+
+const Container = styled.div`
+    text-align:center;
+`
+const Text = styled.div`
+    font-size:25px;
+    margin-top:80px;
+`
+
+const UploadIcon = styled.img`
+    width:50px;
+    height:50px;
+`
+const StyledDiv = styled.div`
+    margin-top:40px;
+    margin-left:-100px;
+`
+const StyledInput = styled.input`
+    position: absolute; 
+    opacity:0;
+    width:160px;
+    height:50px;
+`
+const Text2 = styled.p`
+    position:absolute;
+    font-size:25px;
+    margin-top:10px;
+    display:inline;
+`
+export default Editor
