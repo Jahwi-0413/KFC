f79c1b6976be02ac238b36f88b1ad8ecd8eb2643
jaehe <jaehe0413@naver.com>
2021-04-21 00:17:41 +0900

    Update Editor.js
    파일 등록에 onChange 함수 등록

--- reactapp/src/views/Editor.js
+++ reactapp/src/views/Editor.js
@@ -4,23 +4,29 @@ import icon from '../resources/file-upload-icon.svg'
 
 const Editor = () =>
 {
-    return (
-        <Container>
-            <Text>
-                나만의 손글씨로<br />
+	return (
+		<Container>
+			<Text>
+				나만의 손글씨로<br />
             편리하게 편지를 작성해보세요<br /><br />
             밑의 버튼을 눌러 폰트 파일을 등록하고<br />
             편집기를 사용해 보세요<br />
-            </Text>
-            <StyledDiv>
-                <StyledInput type="file" />
-                <UploadIcon src={icon} />
-                <Text2>파일 선택</Text2>
-            </StyledDiv>
-        </Container>
-    )
+			</Text>
+			<StyledDiv>
+				<StyledInput type="file" accept=".ttf" onChange={uploadFile} />
+				<UploadIcon src={icon} />
+				<Text2>파일 선택</Text2>
+			</StyledDiv>
+		</Container>
+	)
 }
 
+const uploadFile = (e) =>
+{
+	const file = e.target.files[0]
+	if (file !== null)
+		console.log(file)
+}
 const Container = styled.div`
     text-align:center;
 `
