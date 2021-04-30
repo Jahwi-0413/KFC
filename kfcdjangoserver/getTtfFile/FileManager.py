f149c33be886f33973808492322fe6f3147d6d94
jaehe <jaehe0413@naver.com>
2021-04-30 20:24:35 +0900

    Python 파일 tab 간격 설정

--- kfcdjangoserver/getTtfFile/FileManager.py
+++ kfcdjangoserver/getTtfFile/FileManager.py
@@ -3,14 +3,14 @@ import os
 # os.chdir('../../') #작업경로 바꾸는 명령어인데 안먹힘
 base_dir = 'D:/4학년 1학기/창융2/sc/kfcproject/reactapp/src/resources/fonts'#상대경로가 안먹혀서..
 def handle_ttf_file(file, test=False):  
-    save_path = os.path.join(base_dir, file.name)
+  save_path = os.path.join(base_dir, file.name)
 
-    with open(save_path, 'wb+') as destination:
-        for chunk in file.chunks():
-            destination.write(chunk)
+  with open(save_path, 'wb+') as destination:
+    for chunk in file.chunks():
+      destination.write(chunk)
     
-    savedFile = base_dir + '/1.ttf' #reactapp에 있는 ttf파일
-    os.rename(savedFile, base_dir + '/1_used.ttf')      #reactpapp에 있는 ttf파일 이름을 바꾸고
-    removeFile = base_dir + '/1_used.ttf'
-    os.rename(save_path, savedFile)     #사용자가 입력한 ttf파일을 reactapp에 있던 ttf파일로 
-    os.remove(removeFile)
+  savedFile = base_dir + '/1.ttf' #reactapp에 있는 ttf파일
+  os.rename(savedFile, base_dir + '/1_used.ttf')      #reactpapp에 있는 ttf파일 이름을 바꾸고
+  removeFile = base_dir + '/1_used.ttf'
+  os.rename(save_path, savedFile)     #사용자가 입력한 ttf파일을 reactapp에 있던 ttf파일로 
+  os.remove(removeFile)
