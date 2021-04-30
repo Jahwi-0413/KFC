f229368c558800fdfa2759e1cb2f3d7742e7db14
rosf73 <47631768+rosf73@users.noreply.github.com>
2021-04-30 15:29:13 +0900

    템플릿 화면 퍼블리싱

--- reactapp/src/views/Template.js
+++ reactapp/src/views/Template.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import template from '../resources/28-template.jpg'
+import dragin from '../resources/drag-in-icon.png'
+
 function Template() {
   return (
     <Container>
-      <Header>
-        Template
-      </Header>
+      <MainComment>
+        서식을 이용하여 더 정밀한<br/>손글씨 폰트를 만들어 보세요.
+      </MainComment>
+
+      <TemplateMenu>
+        <TemplateDownload>
+          <TemplateImage src={template}/>
+          <DownloadButton>서식 다운로드</DownloadButton>
+        </TemplateDownload>
+        
+        <TemplateUpload>
+          <UploadDragIn>
+            <DragInImage src={dragin}/>
+            <DragInNotice>직접 작성한 서식<br/>이미지를 올려보세요</DragInNotice>
+          </UploadDragIn>
+          <UploadNotice>* 글자 수정없이 잘 보이도록 캡처된 이미지만 올려주세요</UploadNotice>
+        </TemplateUpload>
+      </TemplateMenu>
     </Container>
   );
 }
@@ -14,8 +32,62 @@ function Template() {
 const Container = styled.div`
   text-align: center;
 `;
-const Header = styled.header`
-  text-align: center;
+const MainComment = styled.p`
+  font-size: 35px;
+`;
+const TemplateMenu = styled.div`
+  margin: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const TemplateDownload = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-right: 200px;
+`;
+const TemplateImage = styled.img`
+  width: 160px;
+  border: 1px solid black;
+  margin-bottom: 20px;
+`;
+const DownloadButton = styled.button`
+  border: 0;
+  box-shadow: 2px 2px 3px 1px #666;
+  background: #F3D1D1;
+  font-size: 16px;
+  cursor: pointer;
+  &:focus {
+    outline: none;
+  }
+  padding: 5px 40px;
+`;
+const TemplateUpload = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const UploadDragIn = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 220px;
+  height: 250px;
+  background: lightgray;
+  justify-content: center;
+  align-items: center;
+`;
+const DragInImage = styled.img`
+  width: 40px;
+`;
+const DragInNotice = styled.p`
+  font-size: 15px;
+  color: #333;
+`;
+const UploadNotice = styled.p`
+  font-size: 10px;
+  color: gray;
+  margin-top: 2px;
 `;
 
 export default Template;
