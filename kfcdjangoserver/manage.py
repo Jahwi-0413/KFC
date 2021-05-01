25b7152fdc0ce55d7b24166d79e1059ced842e36
CM-Kim <min1003k@gmail.com>
2021-05-01 21:45:14 +0900

    멀티프로세싱 지원을 위한 freeze_support 추가

--- kfcdjangoserver/manage.py
+++ kfcdjangoserver/manage.py
@@ -2,6 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from multiprocessing import freeze_support
 
 
 def main():
@@ -19,4 +20,5 @@ def main():
 
 
 if __name__ == '__main__':
+    freeze_support()
     main()
