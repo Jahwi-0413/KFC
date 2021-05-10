f50eb6343271c802529b32c206f782391aaa445e
jaehe <jaehe0413@naver.com>
2021-05-11 00:37:26 +0900

    편지지 font size toggle 기능
    버튼은 아직 안꾸밈

--- reactapp/src/components/EditorModal.js
+++ reactapp/src/components/EditorModal.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 import '../index.css';
 
+
+
 function EditorModal ()
 {
+  const fsTypeClass =
+  {
+    1: 'background1',
+    2: 'background2',
+    3: 'background3'
+  }
+
+  const [fsType, setFsType] = useState(fsTypeClass[1])
+
+  const changeFsType = () =>
+  {
+    switch (fsType)
+    {
+      case fsTypeClass[1]: setFsType(fsTypeClass[2]); break;
+      case fsTypeClass[2]: setFsType(fsTypeClass[3]); break;
+      case fsTypeClass[3]: setFsType(fsTypeClass[1]); break;
+    }
+  }
   return (
     <Temp>
       <Container>
-        <StyledArea/>
+        <TextAreaWrapper>
+          <StyledArea className={fsType} />
+        </TextAreaWrapper>
+        <button type="button" onClick={changeFsType}>fontsize</button>
       </Container>
     </Temp>
   );
@@ -27,20 +50,33 @@ const Temp = styled.div`
 const Container = styled.div` 
   display: flex;
   flex-direction: column;
-  width: 35rem;
+  width: 38rem;
   height: 90%;
   background: white;
-  justify-content: center;
-  align-items: center;
+  
   border:1px solid black;
   z-index: 201;
 `;
-const StyledArea = styled.textarea`
-  width: 90%;
+
+// justify-content: center;
+// align-items: center;
+const TextAreaWrapper = styled.div`
+  width: 70%;
   height: 90%;
   font-size: 30px;
   font-family: testfont;
   outline: 0;
+  position:relative;
+  left:20px;
+  top:20px;
+`
+const StyledArea = styled.textarea`
+  width:100%;
+  height:100%;
+  font-family: testfont;
+  background-attachment: local;
+  padding: 8px 10px;
+  resize:none;
 `;
 
 export default EditorModal
