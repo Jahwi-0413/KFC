65cf91144604e581309e7dd5b03c0dce05569755
jaehe <jaehe0413@naver.com>
2021-06-07 00:32:48 +0900

    sentence쪽 문장 변경

--- reactapp/src/views/Sentence.js
+++ reactapp/src/views/Sentence.js
@@ -11,9 +11,9 @@ import { sendSentenceImage } from '../RESTManager';
 
 function Sentence ()
 {
-  const [ modal, setModal ] = useState(false);
-  const [ alert, setAlert ] = useState(false);
-  const [ generated, setGenerated ] = useState("");
+  const [modal, setModal] = useState(false);
+  const [alert, setAlert] = useState(false);
+  const [generated, setGenerated] = useState("");
 
   const onClickNo = () =>
   {
@@ -49,23 +49,27 @@ function Sentence ()
 
   return (
     <Container>
-      {alert && <Alert onClickNo={onClickNo} onClickYes={onClickYes}/>}
+      {alert && <Alert onClickNo={onClickNo} onClickYes={onClickYes} />}
       {modal && (
         generated === "폰트 생성 중" ?
-        <Modal generated={generated}/> :
-        <Modal2 generated={generated} onClickClose={onClickClose}/>
+          <Modal generated={generated} /> :
+          <Modal2 generated={generated} onClickClose={onClickClose} />
       )}
       {modal && (
         generated === "폰트 생성 중" ?
-        <Dimmer onClick={()=>setAlert(true)} /> :
-        <Dimmer onClick={()=>setModal(false)} />
+          <Dimmer onClick={() => setAlert(true)} /> :
+          <Dimmer onClick={() => setModal(false)} />
       )}
       <MainComment>
-        우리의 기술을 이용해 보세요!<br />아주 쉽고 빠르게 만들 수 있습니다.
+        우리의 기술을 이용해 보세요!<br />아주 쉽고 빠르게 만들 수 있습니다.<br />다음의 문장을 손글씨로 작성해 이미지로 올려보세요
       </MainComment>
 
       <SentenceMenu>
-        <SentenceComment>50자 내외로<br />마음껏 작성하여 올려보세요.</SentenceComment>
+        <SentenceComment>
+          옛적 볶은 큰 뻐꾸기 값 품삯 요청을 읽어놓고는 잊었죠<br />
+          특히 쓴 돼지 귀 겉 핥는 내 친구와 앉아 <br />뚫은 돛 옆 부엌에 갇힌 삶 얘기를<br />
+          외곬마냥 읊던 짧은 웨딩 다큐는 권하지 않았습니다<br />
+        </SentenceComment>
         <DaD
           comment={<span>클릭하거나 드래그하여<br />이미지를 올려보세요</span>}
           notice="* 크게, 똑바르게, 수정없이 작성할 수록 폰트가 이뻐집니다"
@@ -91,7 +95,7 @@ const SentenceMenu = styled.div`
 const SentenceComment = styled.p`
   display: inline-block;
   font-size: 25px;
-  margin-right: 100px;
+  margin-right: 60px;
 `;
 
 export default Sentence;
