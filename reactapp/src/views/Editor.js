19d6ba8782ae9ff7a6158d3b76973b5cd136f5e0
jaehe <jaehe0413@naver.com>
2021-06-07 14:39:14 +0900

    Update Editor.js

--- reactapp/src/views/Editor.js
+++ reactapp/src/views/Editor.js
@@ -12,8 +12,8 @@ function Editor (props)
 {
   const comment = <span>클릭하거나 드래그하여<br />폰트 파일을 올려보세요</span>;
   const notice = "* .ttf 확장자의 파일만 업로드 가능합니다";
-  const [ modal, setModal ] = useState(false);  //false가 닫힌거
-  const [ alert, setAlert ] = useState(false);
+  const [modal, setModal] = useState(false);  //false가 닫힌거
+  const [alert, setAlert] = useState(false);
 
   const onClickNo = () =>
   {
@@ -37,19 +37,20 @@ function Editor (props)
       alert('ttf파일을 등록해주세요');
       return
     }
-    sendFontFile(file, setModal);
+    // sendFontFile(file, setModal);
+    setModal(true)
   };
 
   return (
     <Container>
-      {alert && <Alert onClickNo={onClickNo} onClickYes={onClickYes}/>}
+      {alert && <Alert onClickNo={onClickNo} onClickYes={onClickYes} />}
       {modal && <EditorModal />}
-      {modal && <Dimmer onClick={()=>setAlert(true)}></Dimmer>}
+      {modal && <Dimmer onClick={() => setAlert(true)}></Dimmer>}
       <Text>
-        나만의 손글씨로<br/>
-        편리하게 편지를 작성해보세요<br/><br/>
-        밑의 버튼을 눌러 폰트 파일을 등록하고<br/>
-        편집기를 사용해 보세요<br/>
+        나만의 손글씨로<br />
+        편리하게 편지를 작성해보세요<br /><br />
+        밑의 버튼을 눌러 폰트 파일을 등록하고<br />
+        편집기를 사용해 보세요<br />
       </Text>
       <StyledDiv>
         <DaD comment={comment} notice={notice} uploadFile={uploadFile} />
